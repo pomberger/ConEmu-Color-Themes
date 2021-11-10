c17087f0daa193d8efa1aa31f0edc50de6d4dfb0
Hannes Pomberger <pomberger@iem.at>
2021-11-10 13:50:40 +0100

    dump of consol color showing helper scripts/functions

---
+++ Show-Colors.ps1
@@ -0,0 +1,7 @@
+# output all the colour combinations for text/background
+# https://stackoverflow.com/questions/20541456/list-of-all-colors-available-for-powershell/41954792#41954792
+$colors = [enum]::GetValues([System.ConsoleColor])
+Foreach ($bgcolor in $colors) {
+    Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewline }
+    Write-Host " on $bgcolor"
+}
---
+++ test.ps1
@@ -0,0 +1,3 @@
+Write-Host "no newline test " -NoNewline
+Write-Host "second string"
+Write-Host "Red on white text." -ForegroundColor red -BackgroundColor white
